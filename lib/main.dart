987085d86572e208db37819f77d5b39caeb28724
fardintech4 <fardintech4@gmail.com>
2023-11-03 10:07:33 +0530

    second commite

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( NewWidget());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: NewWidget());
-  }
-}
 
 class NewWidget extends StatefulWidget {
   @override
